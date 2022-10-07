e3c348c2fc7180cf5d8439e4529c245ad263fbfd
John <hien.nguyen@ntq-solution.com.vn>
2022-10-07 09:55:32 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { I18n } from "aws-amplify";
+import { translations } from "@aws-amplify/ui-react";
+I18n.putVocabularies(translations);
+I18n.setLanguage("vi");
+I18n.putVocabularies({
+  vi: {
+    "Sign In": "Đăng nhập",
+    "Sign in": "Đăng nhập",
+    "Create Account": "Tạo tài khoản",
+    Password: "Mật khẩu",
+    "Confirm Password": "Nhập lại mật khẩu",
+    Phone: "Số điện thoại",
+    "Forgot your password?": "Quên mật khẩu?",
+    "Reset your password": "Tạo lại mật khẩu",
+    "New Password": "Mật khẩu mới",
+    Code: "Mã",
+    Submit: "Gửi",
+    "Enter your email": "Nhập lại email của bạn",
+    "Send code": "Gửi mã",
+    "Resend Code": "Gửi lại mã",
+    "Back to Sign In": "Quay lại trang đăng nhập",
+  },
+});
 
-function App() {
+const formFields = {
+  signUp: {
+    phone_number: {
+      dialCodeList: ["+84"],
+    },
+  },
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator formFields={formFields}>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
   );
 }
-
-export default App;
